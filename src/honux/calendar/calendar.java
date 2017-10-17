fd88067929391565bb581ac0c8dc983b7ee8256e
Administrator <Administrator@DESKTOP-AS96FL8>
2017-10-18 02:08:32 +0900

    숫자를 입력받아 해당하는 달의 최대 일수를 출력

--- src/honux/calendar/calendar.java
+++ src/honux/calendar/calendar.java
@@ -1,9 +1,21 @@
 package honux.calendar;
 
+import java.util.Scanner;
+
 public class calendar {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, Calendar");
+		
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("달을 입력하세요");
+		int month = scanner.nextInt();
+		
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.printf("%d월은 %d일까지 있습니다. \n", month, maxDays[month - 1]);
+		scanner.close();
 	}
 
 }
